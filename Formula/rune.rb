c8314d901d0b8d725d8ea077f8447e89ba49ce4c
Siddhartha Khuntia <77291662+sidkhuntia@users.noreply.github.com>
2025-06-14 04:24:13 +0530

    rune 1.0.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/rune.rb
+++ Formula/rune.rb
@@ -1,13 +1,13 @@
 class Rune < Formula
   desc "AI-powered Git commit message generator"
   homepage "https://github.com/sidkhuntia/homebrew-rune"
-  version "1.0.2"
+  version "1.0.4"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/sidkhuntia/homebrew-rune/releases/download/v1.0.2/rune-darwin-arm64.tar.gz"
-      sha256 "sha256:79b87727dc06efedc00a62c9bbf941fd950778b0441c87ad650661849ee44495"
+      url "https://github.com/sidkhuntia/homebrew-rune/archive/v1.0.4.tar.gz"
+      sha256 "0c758cb4ac4f376ded6ee9dcd6406a931ad423b6df4dd3595a5f132f7545bce3"
     else
       url "https://github.com/sidkhuntia/homebrew-rune/releases/download/v1.0.2/rune-darwin-amd64.tar.gz"
       sha256 "sha256:2cc1f35d1c65242e0096c79fec944c79c1927de29726fa3de76517cae9bfa568"
