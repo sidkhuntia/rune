6039490af39a574b6f661ffac97793a63f899f07
GitHub Action <action@github.com>
2025-06-14 07:30:24 +0000

    Update Homebrew formula to v1.0.8

--- Formula/rune.rb
+++ Formula/rune.rb
@@ -1,16 +1,16 @@
 class Rune < Formula
   desc "AI-powered Git commit message generator"
   homepage "https://github.com/sidkhuntia/rune"
-  version "1.0.7"
+  version "1.0.8"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/sidkhuntia/rune/releases/download/v1.0.7/rune-darwin-arm64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/sidkhuntia/rune/releases/download/v1.0.8/rune-darwin-arm64.tar.gz"
+      sha256 "2a5bb9919b6868e41ed131fd0a865f7a755533603ed1eb8494d3777ceb58da37"
     else
-      url "https://github.com/sidkhuntia/rune/releases/download/v1.0.7/rune-darwin-amd64.tar.gz"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/sidkhuntia/rune/releases/download/v1.0.8/rune-darwin-amd64.tar.gz"
+      sha256 "c39f454bd55469f94d332a095d52a37e4fcc595b59bbea8edff147b4e8047462"
     end
   end
 
