bd73c3fe36a8e2a8b66f302420a1f2b472419cdf
Siddhartha Khuntia <siddharthakhuntia@gmail.com>
2025-06-14 12:40:51 +0530

    chore: enhance CI workflow for Homebrew formula updates
    - Replaced Homebrew formula update method to dynamically fetch release asset SHA256 hashes.
    - Updated formula file generation to reflect the correct repository URL and versioning.
    - Improved commit process for updated formula to ensure proper versioning and SHA256 integrity.

--- Formula/rune.rb
+++ Formula/rune.rb
@@ -1,15 +1,15 @@
 class Rune < Formula
   desc "AI-powered Git commit message generator"
-  homepage "https://github.com/sidkhuntia/homebrew-rune"
+  homepage "https://github.com/sidkhuntia/rune"
   version "1.0.2"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/sidkhuntia/homebrew-rune/releases/download/v1.0.2/rune-darwin-arm64.tar.gz"
+      url "https://github.com/sidkhuntia/rune/releases/download/v1.0.2/rune-darwin-arm64.tar.gz"
       sha256 "sha256:79b87727dc06efedc00a62c9bbf941fd950778b0441c87ad650661849ee44495"
     else
-      url "https://github.com/sidkhuntia/homebrew-rune/releases/download/v1.0.2/rune-darwin-amd64.tar.gz"
+      url "https://github.com/sidkhuntia/rune/releases/download/v1.0.2/rune-darwin-amd64.tar.gz"
       sha256 "sha256:2cc1f35d1c65242e0096c79fec944c79c1927de29726fa3de76517cae9bfa568"
     end
   end
